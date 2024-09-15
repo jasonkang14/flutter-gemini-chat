37e066450f4e35bc8152dff105084573e7dc89f4
Jason Kang <jasonkang14@gmail.com>
2024-09-15 12:51:05 +0900

    add a text input

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini_chat/ai_message.dart';
 import 'package:flutter_gemini_chat/human_message.dart';
+import 'package:flutter_svg/svg.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,18 +32,55 @@ class ChatPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 32, 16, 74),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('hey'),
-            AIMessage(
-                content:
-                    'Really love your most recent photo. I\'ve been trying to capture the same thing for a few months and would love some tips!'),
-            SizedBox(height: 24),
-            HumanMessage(
-                content:
-                    'A fast 50mm like f1.8 would help with the bokeh. I\'ve been using primes as they tend to get a bit sharper images.'),
+            const SingleChildScrollView(
+              child: Column(
+                children: [
+                  AIMessage(
+                      content:
+                          'Really love your most recent photo. I\'ve been trying to capture the same thing for a few months and would love some tips!'),
+                  SizedBox(height: 24),
+                  HumanMessage(
+                      content:
+                          'A fast 50mm like f1.8 would help with the bokeh. I\'ve been using primes as they tend to get a bit sharper images.'),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Type a message',
+                      hintStyle: const TextStyle(
+                        color: Color(0xFF656565),
+                        fontSize: 14,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xFFF7F7FC),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                IconButton(
+                  icon: SvgPicture.asset('assets/icons/send-chat.svg'),
+                  onPressed: () {},
+                )
+              ],
+            ),
           ],
         ),
       ),
