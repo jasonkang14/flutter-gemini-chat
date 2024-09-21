189df2a24d35f7ba0fbbb44dd8133a9480a4519f
Jason Kang <jasonkang14@gmail.com>
2024-09-21 15:52:28 +0900

    Add images

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -24,6 +24,78 @@ class ChatPage extends StatelessWidget {
           ),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 32,
+        ),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset('assets/images/ai.png'),
+                const SizedBox(width: 16),
+                const ChatMessage(
+                  isHuman: false,
+                  message:
+                      'Really love your most recent photo. I’ve been trying to capture the same thing for a few months and would love some tips!',
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const ChatMessage(
+                  message:
+                      'A fast 50mm like f1.8 would help with the bokeh. I’ve been using primes as they tend to get a bit sharper images.',
+                  isHuman: true,
+                ),
+                const SizedBox(width: 16),
+                Image.network('assets/images/human.png'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({
+    super.key,
+    required this.message,
+    required this.isHuman,
+  });
+
+  final String message;
+  final bool isHuman;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.03),
+          borderRadius: BorderRadius.only(
+            topLeft: isHuman ? const Radius.circular(6) : Radius.zero,
+            topRight: isHuman ? Radius.zero : const Radius.circular(6),
+            bottomLeft: const Radius.circular(6),
+            bottomRight: const Radius.circular(6),
+          ),
+        ),
+        child: Text(
+          message,
+          style: const TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.normal,
+            height: 18 / 13,
+          ),
+        ),
+      ),
     );
   }
 }
